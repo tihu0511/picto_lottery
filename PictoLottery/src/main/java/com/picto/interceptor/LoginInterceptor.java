f68a28287e2452e6905d6a14f2730e8497b6ec65
BF100271 <huozhanbai@baofoo.com>
2016-05-24 19:02:21 +0800

    拦截器

---
+++ PictoLottery/src/main/java/com/picto/interceptor/LoginInterceptor.java
@@ -0,0 +1,10 @@
+package com.picto.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Created by BF100271 on 2016/5/24.
+ */
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    //TODO 登录拦截
+}
---
+++ PictoLottery/src/main/java/com/picto/interceptor/MenuAuthInterceptor.java
@@ -0,0 +1,10 @@
+package com.picto.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Created by BF100271 on 2016/5/24.
+ */
+public class MenuAuthInterceptor extends HandlerInterceptorAdapter {
+    // TODO 菜单权限拦截
+}
